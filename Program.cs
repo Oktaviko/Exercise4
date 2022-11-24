acae5bdb753f1d7de11d6ce23ecb188e303985d1
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-11-24 16:01:14 +0700

    add the display menu

--- Program.cs
+++ Program.cs
@@ -68,6 +68,21 @@ namespace Exercise4
         static void Main(string[] args)
         {
             CircularList obj = new CircularList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. View all the records in the list");
+                    Console.WriteLine("2. Search for a record in the list");
+                    Console.WriteLine("3. Display the first record in the list");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("Enter your choice (1-4) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+
+
+                }
+            }
         }
     }
 }
